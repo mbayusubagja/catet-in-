e6c6bc622b126b1afb903d1c7a1d4325eb16c3c8
Muhammad Bayu Subagja <66833650+mbayusubagja@users.noreply.github.com>
2026-07-14 21:48:36 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://zdxpumcwrwhjhccrfzkc.supabase.co";
-const SUPABASE_KEY = "sb_publishable_sjPFHswpRP9h0X9QtbO04Q_stoJNsD5";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
